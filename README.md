4a4c2581b1f88bebd30457ca92dc6dfafe151fa8
Kauane <kauanembordin@gmail.com>
2024-10-16 17:57:29 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# BAF_eco_strategies
+This repository contains the data and codes from the following manuscript:
+
+**Distinct ecological strategies predict tree growth across the subtropical Brazilian Atlantic Forest**
+
+Repository content:
+
+The `scripts` folder contains:
+
+Scripts to perform all the analyses from the manuscript\
+*code.R* - packages and functions, data visualisation, PCAs and linear models.
+
+The `data` folder contains:
+
+Functional trait data and relative growth rates for all the 123 species\
+species.information.csv - csv. file containing data, see description of each column below
+
+metadata.xlxs: metadata (readme file) with the same information as described below
+
+Species.code: individual code for each species based on the scientific name
+
+Species: species name
+
+Family: Species family
+
+Mean.absolute.growth.rate: mean species growth rate based on diameter (mm) in time 1 minus diameter in time 0, divided by census interval (in years) \<-\> (dbh1-dbh0)/time
+
+SD.growth.rate: standard deviation from the mean absolute growth rates
+
+SLA: specific leaf area, in cm2/g
+
+LA: leaf area, in cm2
+
+LDMC: leaf dry-matter content, in mg/g
+
+LNC: leaf nitrogen content, in %
+
+LPC: leaf phosphorus content, in %
+
+WD: wood density, in g/cm3
+
+Height: maximum height per species within the sampled trees, in meters
+
+Number.of.stems: number of stems per species
+
+Median.height: species median height within the sampled trees, in meters
+
+Category: canopy or understory classification
+
+Relative.Growth.Rate: species relative growth rate, based on the following equation: (dbht1/dbhto)/t
+
+The `results` folder contains:
+
+*results.txt:* results from the models fitted
+
+*model.selection.txt*: results from the model selection
+
+PC1 a PC3.png: PCAs diagrams
+
+The code was developed and tested using the R statistical software, R version 4.2.2 (R Development Core Team, 2022).
---
+++ scripts/code.R
@@ -0,0 +1,212 @@
+# load packages and functions -----
+library(here)
+library(doBy)
+library(reshape2)
+library(tidyr)
+library(picante)
+library(nlme)
+library(corrplot)
+library(lme4)
+library(MuMIn)
+library(tidyverse)
+library(ggfortify)
+library(lmerTest)
+library(visreg)
+library(ggpubr)
+library(FactoMineR)
+library(dplyr)
+
+models <- function(data.models, variable){
+  model0 <- list()
+  model1 <- list()
+  model2 <- list()
+  model3 <- list()
+  model4 <- list()
+  model5 <- list()
+  model6 <- list()
+  model7 <- list()
+  
+  model.selection <- NULL
+  
+  if(variable== "rgr"){
+    m0 <- lm(log(rgr) ~1, data = data.models)
+    m1 <- lm(log(rgr)~PC1, data=data.models)
+    m2 <- lm(log(rgr)~PC2, data=data.models)
+    m3 <- lm(log(rgr)~PC3, data=data.models)
+    m4 <- lm(log(rgr)~PC1+PC2, data=data.models)
+    m5 <- lm(log(rgr)~PC1+PC2+PC3, data=data.models)
+    m6 <- lm(log(rgr)~PC1+PC3, data = data.models)
+    m7 <- lm(log(rgr)~PC2+PC3, data = data.models)
+  }
+  model.selection = as.data.frame(model.sel(m0, m1,m2,m3,m4,m5,m6,m7))
+  model0 <- m0
+  model1 <- m1
+  model2 <- m2
+  model3 <- m3
+  model4 <- m4
+  model5 <- m5
+  model6 <- m6
+  model7 <- m7
+  
+  return(list(model.selection = model.selection, 
+              m0=model0,
+              m1=model1,
+              m2=model2,
+              m3=model3,
+              m4=model4,
+              m5=model5,
+              m6=model6,
+              m7=model7))
+}
+pca.vis <- function(data,pca,axis1,axis2,name){
+  autoplot(pca, 
+           label= F, # if T, size= 0
+           data=data, #dados originais inseridos na pca
+           colour="log.gr.dbh", #coluna que indica os grupos de cores
+           label.size=4, #tamanho dos rotulos
+           shape="Forest.stratum", #omite o simbolo
+           size="growth_dbh",
+           loadings=TRUE, #inclui as setas com as variaveis
+           loadings.label.repel = TRUE,
+           loadings.colour="black", #edita a cor das setas
+           loadings.label=TRUE, #mostra o rotulo das variaveis
+           loadings.label.size=4, #altera o tamanho do rotulo das variaveis
+           loadings.label.colour="black", #altera cor do rotulo das variaveis
+           x=axis1, y=axis2) + #PC to be plotted
+    my_theme+
+    theme(aspect.ratio=1)+
+    theme(text = element_text(size=15))+
+    scale_colour_gradient(low = "#F0E442",high = "#009E73")+
+    geom_vline(xintercept=0, color="gray80", linetype="dotted") +
+    geom_hline(yintercept=0, color="gray80", linetype="dotted") + 
+    xlim(-0.4,0.4)+
+    theme(plot.title=element_text(size=14,face="bold",hjust = 0.5))+
+    labs(title=name)
+  
+}
+my_theme <- theme_light() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank()) +
+  theme(legend.position = "none",
+        text = element_text(size=15),
+        plot.margin=unit(c(0.1, 1, 0.1 , 0.1), "cm"),
+        axis.text.x = element_text(size=15),
+        axis.text.y = element_text(size = 15)) 
+
+# load dataset -----
+data <- read.csv(here::here("data", "species.information.csv"))
+
+data <- data %>%
+  mutate(rgr = Relative.Growth.Rate,
+         LA = log(LA))
+
+# data filter -----
+dat.total = data %>% 
+  mutate(Forest.stratum=Category)
+dat.underst = data %>% 
+  filter(Category == "Understory") %>% 
+  mutate(Forest.stratum=Category)
+dat.canop = data %>% 
+  filter(Category == "Canopy") %>% 
+  mutate(Forest.stratum=Category)
+
+# PCAs -----
+pca.tot <- dat.total %>% 
+  select(WD,SLA,LNC,LPC,LDMC,Height,LA)
+pca.tot <- prcomp((pca.tot), center=T, scale=T) 
+pca.tot$rotation; summary(pca.tot)
+
+pca.und <- dat.underst %>% 
+  select(WD,SLA,LNC,LPC,LDMC,Height,LA)
+pca.und <- prcomp((pca.und), center=T, scale=T) 
+pca.und$rotation; summary(pca.und)
+
+pca.canop <- dat.canop %>% 
+  select(WD,SLA,LNC,LPC,LDMC,Height,LA)
+pca.canop <- prcomp((pca.canop), center=T, scale=T) 
+pca.canop$rotation; summary(pca.canop)
+
+# data for analysis ----
+dat.total <- dat.total %>%
+  mutate(PC1 = (pca.tot$x[,1])) %>%
+  mutate(PC2 = (pca.tot$x[,2])) %>%
+  mutate(PC3 = (pca.tot$x[,3])) 
+
+dat.underst <- dat.underst %>%
+  mutate(PC1 = (pca.und$x[,1])) %>%
+  mutate(PC2 = (pca.und$x[,2])) %>%
+  mutate(PC3 = (pca.und$x[,3]))
+
+dat.canop <- dat.canop %>%
+  mutate(PC1 = (pca.canop$x[,1])) %>%
+  mutate(PC2 = (pca.canop$x[,2])) %>%
+  mutate(PC3 = (pca.canop$x[,3]))
+
+# growth models -----
+set.seed(999) 
+
+# all 
+data.models <- dat.total 
+total <- models(data.models = data.models, variable = "rgr")
+total.modelselection <- total$model.selection %>%  mutate(resu.from = "total.model")
+total_best.model <- total$m7 #parcimonia 
+boot.total = car::Boot(total_best.model, R=9999)
+CI.total = confint(boot.total); CI.total
+summary(total_best.model)
+r2.total = RsquareAdj(total_best.model)
+results.total <- broom::tidy(total_best.model) %>%
+  mutate(`CI 2.5%` = confint(boot.total)[,1],
+         `CI 97.5%` = confint(boot.total)[,2])%>%
+  slice(-1) %>% 
+  mutate(`R2 adj` = r2.total$adj.r.squared, 
+         resu.from = "total.model"); results.total
+
+#understory
+data.models <- dat.underst
+unders <- models(data.models = data.models, variable = "rgr")
+unders.modelselection <- unders$model.selection%>%  mutate(resu.from = "understory.model")
+unders_best.model <- unders$m2 #parcimonia
+boot.unders = car::Boot(unders_best.model, R=9999)
+CI.unders = confint(boot.unders); CI.unders
+summary(unders_best.model)
+r2.und = RsquareAdj(unders_best.model)
+results.und <- broom::tidy(unders_best.model) %>%
+  mutate(`CI 2.5%` = confint(boot.unders)[,1],
+         `CI 97.5%` = confint(boot.unders)[,2])%>%
+  slice(-1) %>% 
+  mutate(`R2 adj` = r2.und$adj.r.squared, 
+         resu.from = "understory.model"); results.und
+
+#canopy
+data.models <- dat.canop
+canop <- models(data.models = data.models, variable = "rgr")
+canop.modelselection <- canop$model.selection %>%  mutate(resu.from = "canopy.model")
+canop_best.model <- canop$m6
+boot.canop = car::Boot(canop_best.model, R=9999)
+CI.canop = confint(boot.canop); CI.canop
+summary(canop_best.model)
+r2.can = RsquareAdj(canop_best.model)
+results.can <- broom::tidy(canop_best.model) %>%
+  mutate(`CI 2.5%` = confint(boot.canop)[,1],
+         `CI 97.5%` = confint(boot.canop)[,2])%>%
+  slice(-1) %>% 
+  mutate(`R2 adj` = r2.can$adj.r.squared, 
+         resu.from = "canopy.model"); results.can
+
+results = bind_rows(results.total,results.und,results.can)
+model.sel = bind_rows(total.modelselection, unders.modelselection,canop.modelselection)
+#write.table(results, "results/results.txt")
+#write.table(model.sel, "results/model.selection.txt")
+
+# PCA plots ----
+a <- pca.vis(data = dat.total,pca = pca.tot,axis1 = 1,axis2 = 2, name = "All")+labs(tag="a)")
+b <- pca.vis(data = dat.total,pca = pca.tot,axis1 = 1,axis2 = 3, name = "All")+labs(tag="d)")
+c <- pca.vis(data = dat.underst,pca = pca.und,axis1 = 1,axis2 = 2, name = "Understory")+labs(tag="c)")+scale_shape_manual(values = 17)
+d <- pca.vis(data = dat.underst,pca = pca.und,axis1 = 1,axis2 = 3, name = "Understory")+labs(tag="f)")+scale_shape_manual(values = 17)
+e <- pca.vis(data = dat.canop,pca = pca.canop,axis1 = 1,axis2 = 2, name = "Canopy")+labs(tag="b)")
+f <- pca.vis(data = dat.canop,pca = pca.canop,axis1 = 1,axis2 = 3, name = "Canopy")+labs(tag="e)")
+
+#png('results/PC1 a PC3.png', units="in", width=13, height=8, res=300)
+gridExtra:: grid.arrange(a,e,c,b,f,d, ncol=3)
+#dev.off()
